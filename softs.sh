fb83f2978a1214925234d3662c2cb8e0c5652428
Devops <109498305+projectaws741@users.noreply.github.com>
2025-01-02 14:08:40 +0530

    Update softs.sh

--- softs.sh
+++ softs.sh
@@ -4,6 +4,7 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade -y
 # Add required dependencies for the jenkins package
+sudo yum install java-17-amazon-corretto
 sudo yum install fontconfig -y
 sudo yum install jenkins -y
 sudo systemctl daemon-reload
