104912a372ccc3f6a5f7d9d79aeeb43df47f2ca3
Devops <109498305+projectaws741@users.noreply.github.com>
2025-01-02 10:39:42 +0530

    Create softs.sh

---
+++ softs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade -y
+# Add required dependencies for the jenkins package
+sudo yum install fontconfig -y
+sudo yum install jenkins -y
+sudo systemctl daemon-reload
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+sudo yum install git -y
